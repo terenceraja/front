5e1a86e9694d72e107d2d894b1cce296707cb11a
Terence Rajapaksage <terenceraja@hotmail.fr>
2023-04-27 16:15:23 +0200

    tweet content continué

--- components/TweetMain.js
+++ components/TweetMain.js
@@ -1,7 +1,39 @@
 import styles from "../styles/TweetMain.module.css";
+import { Button, Modal } from "antd";
 
 function TweetMain() {
-  return <div>HELLO</div>;
+  return (
+    <div className={styles.mainDiv}>
+      <div className={styles.leftDiv}>
+        <div className={styles.logoContent}>
+          <img className={styles.logoImageRight} src="/logo1.png" alt="logo" />
+        </div>
+
+        <div className={styles.userContent}>
+          <div>USER</div>
+          <Button type="tweetBtn">Tweet</Button>
+        </div>
+      </div>
+
+      <div className={styles.CenterDiv}>
+        <div className={styles.CenterTopDiv}>
+          <h1 className={styles.homeTxt}>HOME</h1>
+
+          <input
+            className={styles.tweetInput}
+            type="text"
+            placeholder="What's up?"
+          />
+
+          <div className={styles.tweetContent}>
+            <span>counter</span>
+            <Button type="tweetBtn">Tweet</Button>
+          </div>
+        </div>
+      </div>
+      <div className={styles.rightDiv}></div>
+    </div>
+  );
 }
 
 export default TweetMain;
